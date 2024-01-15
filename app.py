2d34ae5b23af045cb019f1715b86ed79b9925ccd
Hiroshan <ihiroshan@outlook.com>
2024-01-15 05:40:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,18 +18,19 @@ def hello():
 @app.route('/recipe', methods=["GET"])
 def recommend_recipe():
     ingredients = request.args.get('ingredients')
-    print(ingredients)
-    recipes = getRecipeRecommendations(5, [ingredients])
-    print(recipes)
-    response = {}
-    for index, row in recipes.iterrows():
-        response[index] = {
-                            'recipe': str(row['recipe_name']),
-                            'score': str(row['scores']),
-                            'ingredients': str(row['ingredients']),
-                            'url': str(row['recipe_urls'])
-                          }        
-    return jsonify(response)
+
+    return jsonify({"Ingredients":ingredients})
+    # recipes = getRecipeRecommendations(5, [ingredients])
+    # print(recipes)
+    # response = {}
+    # for index, row in recipes.iterrows():
+    #     response[index] = {
+    #                         'recipe': str(row['recipe_name']),
+    #                         'score': str(row['scores']),
+    #                         'ingredients': str(row['ingredients']),
+    #                         'url': str(row['recipe_urls'])
+    #                       }        
+    # return jsonify(response)
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", debug=False)
